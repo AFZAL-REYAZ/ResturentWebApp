1a0364e210b220d1f709c19cb37941875b361806
afzal reyaz <afjalreyaz@gmail.com>
2024-01-19 17:35:44 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import AddToCart from './components/AddToCart';
+import FilterdPage from './components/FilterdPage';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/filteredpage/:category' element={<FilterdPage/>}/>
+        <Route path='/addtocart' element={<AddToCart/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
